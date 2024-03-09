09a773b79e44e4a641e263da4555b2a29897218b
Justin Owolabi <157639024+JAOwolabi@users.noreply.github.com>
2024-03-08 18:37:57 -0600

    scores

--- scores.c
+++ scores.c
@@ -6,7 +6,7 @@
 #define TOUCHDOWN_2PT 8
 #define TOUCHDOWN_1PT 7
 
-void printScoringPlays(int td, int fg, int safety, int td_2pt, int td_1pt) {
+void Plays(int td, int fg, int safety, int td_2pt, int td_1pt) {
     if (td > 0) printf("%d TD ", td);
     if (td_2pt > 0) printf("%d TD + 2pt ", td_2pt);
     if (td_1pt > 0) printf("%d TD + 1pt FG ", td_1pt);
@@ -15,17 +15,17 @@ void printScoringPlays(int td, int fg, int safety, int td_2pt, int td_1pt) {
     printf("\n");
 }
 
-void findScoringCombos(int score, int td, int fg, int safety, int td_2pt, int td_1pt) {
+void Scoring(int score, int td, int fg, int safety, int td_2pt, int td_1pt) {
     if (score == 0) {
-        printScoringPlays(td, fg, safety, td_2pt, td_1pt);
+        Plays(td, fg, safety, td_2pt, td_1pt);
         return;
     }
 
-    if (score >= TOUCHDOWN) findScoringCombos(score - TOUCHDOWN, td + 1, fg, safety, td_2pt, td_1pt);
-    if (score >= FIELD_GOAL) findScoringCombos(score - FIELD_GOAL, td, fg + 1, safety, td_2pt, td_1pt);
-    if (score >= SAFETY) findScoringCombos(score - SAFETY, td, fg, safety + 1, td_2pt, td_1pt);
-    if (score >= TOUCHDOWN_2PT) findScoringCombos(score - TOUCHDOWN_2PT, td, fg, safety, td_2pt + 1, td_1pt);
-    if (score >= TOUCHDOWN_1PT) findScoringCombos(score - TOUCHDOWN_1PT, td, fg, safety, td_2pt, td_1pt + 1);
+    if (score >= TOUCHDOWN) Scoring(score - TOUCHDOWN, td + 1, fg, safety, td_2pt, td_1pt);
+    if (score >= FIELD_GOAL) Scoring(score - FIELD_GOAL, td, fg + 1, safety, td_2pt, td_1pt);
+    if (score >= SAFETY) Scoring(score - SAFETY, td, fg, safety + 1, td_2pt, td_1pt);
+    if (score >= TOUCHDOWN_2PT) Scoring(score - TOUCHDOWN_2PT, td, fg, safety, td_2pt + 1, td_1pt);
+    if (score >= TOUCHDOWN_1PT) Scoring(score - TOUCHDOWN_1PT, td, fg, safety, td_2pt, td_1pt + 1);
 }
 
 int main() {
@@ -38,7 +38,7 @@ int main() {
 
         if (score > 1) {
             printf("Possible combinations of scoring plays:\n");
-            findScoringCombos(score, 0, 0, 0, 0, 0);
+            Scoring(score, 0, 0, 0, 0, 0);
         }
     } while (score > 1);
 
